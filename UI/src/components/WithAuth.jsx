d9d1fb2fbdeec409b0e4fa6133db10b946b7e59b
Purushottam Khedre <purushottamkhedre123@gmail.com>
2023-12-26 16:45:30 +0530

    WithAuth and some css changes

--- UI/src/components/WithAuth.jsx
+++ UI/src/components/WithAuth.jsx
@@ -1,30 +1,26 @@
-import React, { useEffect } from 'react'
-import Cookies from 'js-cookie';
+import React, { useEffect } from "react";
+import Cookies from "js-cookie";
 import { jwtDecode } from "jwt-decode";
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const WithAuth = (props) => {
-    const history = useHistory();
+  const history = useHistory();
 
-    useEffect(() => {
-     const  cookie_token = localStorage.getItem('token');
-        // const cookie_token = Cookies.get('cookie_token');
-        console.log("drgdfgdf", cookie_token);
-        if (cookie_token) {
-            const decodedToken = jwtDecode(cookie_token);
-            const userType = decodedToken.role;
-            if (userType === 'user') {
-                history.push('/login');
-              } 
-        }else{
-                history.push('/shop');
-              } 
-    }, [])
-    return (
-        <>
-            {props.children}
-        </>
-    )
-}
+  useEffect(() => {
+    const cookie_token = localStorage.getItem("token");
+    // const cookie_token = Cookies.get('cookie_token');
+    console.log("drgdfgdf", cookie_token);
+    if (cookie_token) {
+      const decodedToken = jwtDecode(cookie_token);
+      const userType = decodedToken.role;
+      if (userType === "user") {
+        // history.push('/login');
+      }
+    } else {
+      history.push("/shop");
+    }
+  }, []);
+  return <>{props.children}</>;
+};
 
-export default WithAuth
+export default WithAuth;
--- UI/src/router/Admin/Page.jsx
+++ UI/src/router/Admin/Page.jsx
@@ -8,70 +8,53 @@ import {
   useRouteMatch,
 } from "react-router-dom/cjs/react-router-dom.min";
 import ProductsPage from "./Products/Page";
-<<<<<<< HEAD
-import WithAuth from "@/components/WithAuth";
-=======
 import CustomizationPage from "./Customizations/Page";
-import "./styles.css";
+// import "./styles.css";
 import BestSellerProduct from "./BestSellerProduct/Page";
 import Reviews from "./Reviews/Page";
 import FAQs from "./Faqs/Page";
 import Blogs from "./Blogs/Page";
->>>>>>> 1d35e824ed2ac228f2fdf71ce15b45c492fda732
+import WithAuth from "@/components/WithAuth";
 
 const Page = () => {
   let match = useRouteMatch();
   return (
     <WithAuth>
-    <AdminContext>
-<<<<<<< HEAD
-      <Layout Page={
-        <Switch>
-          <Route path={`${match.path}/products`}>
-            <ProductsPage />
-          </Route>
+      <AdminContext>
+        <Layout
+          Page={
+            <Switch>
+              <Route path={`${match.path}/products`}>
+                <ProductsPage />
+              </Route>
 
-          <Route path={`${match.path}/:id`}>
-            <Home />
-          </Route>
-        </Switch>
-      } />
-    </AdminContext></WithAuth>
-=======
-      <Layout
-        Page={
-          <Switch>
-            <Route path={`${match.path}/products`}>
-              <ProductsPage />
-            </Route>
+              <Route path={`${match.path}/customizations`}>
+                <CustomizationPage />
+              </Route>
 
-            <Route path={`${match.path}/customizations`}>
-              <CustomizationPage />
-            </Route>
+              <Route path={`${match.path}/best-seller`}>
+                <BestSellerProduct />
+              </Route>
 
-            <Route path={`${match.path}/best-seller`}>
-              <BestSellerProduct />
-            </Route>
+              {/* create a route for review */}
+              <Route path={match.path + "/reviews"}>
+                <Reviews />
+              </Route>
 
-            {/* create a route for review */}
-            <Route path={match.path + "/reviews"}>
-              <Reviews />
-            </Route>
+              <Route path={`${match.path}/faqs`}>
+                <FAQs />
+              </Route>
 
-            <Route path={`${match.path}/faqs`}>
-              <FAQs />
-            </Route>
+              <Route path={match.path + "/blogs"}>
+                <Blogs />
+              </Route>
 
-            <Route path={match.path + "/blogs"}>
-              <Blogs />
-            </Route>
-
-            <Route path={`${match.path}/:id`}>{/* <Editor /> */}</Route>
-          </Switch>
-        }
-      />
-    </AdminContext>
->>>>>>> 1d35e824ed2ac228f2fdf71ce15b45c492fda732
+              <Route path={`${match.path}/:id`}>{/* <Editor /> */}</Route>
+            </Switch>
+          }
+        />
+      </AdminContext>
+    </WithAuth>
   );
 };
 
