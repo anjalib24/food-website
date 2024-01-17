4570b66e116abd6233a3a76908f845b0bd59ae89
Rupal <shakileverpeak@gmail.com>
2024-01-17 14:26:26 +0530

    comment helmet and compression

--- app/src/app.js
+++ app/src/app.js
@@ -27,8 +27,8 @@ const app = express();
 // }
 
 app.use(cors());
-app.use(helmet());
-app.use(compression());
+// app.use(helmet());
+// app.use(compression());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
