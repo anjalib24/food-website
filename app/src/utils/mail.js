b613c3acddc1026910f00217d80d2411a801af6c
Rupal <shakileverpeak@gmail.com>
2024-02-08 13:35:36 +0530

    fixed

--- app/src/utils/mail.js
+++ app/src/utils/mail.js
@@ -207,7 +207,7 @@ const sendResetPasswordEmail = async (recipientEmail, name, userId, token) => {
     const emailBannerDataURI = readImageAsDataURI(emailBannerPath);
 
     // const localResetLink = `http://localhost:5173/reset-password/${token}`;
-    const resetLink = `https://ethnicfoods.com//reset-password/${token}`;
+    const resetLink = `https://ethnicfoods.com/reset-password/${token}`;
 
     const htmlContent = `
       <div style="max-width: 600px; margin: 0 auto; font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px;">
